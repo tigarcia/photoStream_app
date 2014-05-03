1526e2eca939cf497119fabde6f36fb6b1a4fa5c
courtney mcilhenny <github@courtneymcilhenny>
2014-05-03 16:54:12 -0700

    First commit on PhotoStream app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhotoStream::Application.config.secret_key_base = 'd7c1fb4c95229a54218268ff12c019f3e3665b36deeb3f0bfc6cd669f78b2d70a29d49ac12b8b03bde2849dbe68776d43a4b5b1f9ea9225bc17b58a30e1030ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhotoStream::Application.config.session_store :cookie_store, key: '_photoStream_session'
