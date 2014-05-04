d67be03a021ea260a9071d097c5c4a7a4ba9f0b4
courtney mcilhenny <github@courtneymcilhenny>
2014-05-04 16:56:34 -0700

    Adding Event model, migration and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+events = Event.create ([
+  {name: 'Birthday', event_date: 20140520 },
+  {name: 'Happy Hour', event_date: 20140604},
+  {name: '50th Anniversary', event_date: 20140515}
+
+  ])
