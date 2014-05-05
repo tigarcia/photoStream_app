5dccd46578ac8f9327a923e7f6e063de5cefae42
courtney mcilhenny <github@courtneymcilhenny>
2014-05-04 18:20:21 -0700

    Implemented event show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,10 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   private
     def event_params
       params.require(:event).permit(:name, :event_date)
